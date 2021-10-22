319f86c6527f733e57670c09b53c89f2d68d35d6
Raphael Campos <raphael.campos@streamer.com.br>
2021-10-21 22:29:55 -0300

    Add project files.

---
+++ AluraTestsCourse.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using AluraTestsCourse.LeilaoOnline.Core;
+
+namespace AluraTestsCourse.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        private static void Verifica(double esperado, double obtido)
+        {
+            if (esperado == obtido)
+            {
+                Console.WriteLine("TESTE OK");
+            }
+            else Console.WriteLine("TESTE FALHOU");
+        }
+
+        private static void LeilaoComVariosLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 700);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void LeilaoComApenasUmLance()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+
+        }
+    }
+}
---
+++ AluraTestsCourse.LeilaoOnline.Tests/LeilaoTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AluraTestsCourse.LeilaoOnline.Core;
+using Xunit;
+
+namespace AluraTestsCourse.LeilaoOnline.Tests
+{
+    public class LeilaoTests
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 700);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
