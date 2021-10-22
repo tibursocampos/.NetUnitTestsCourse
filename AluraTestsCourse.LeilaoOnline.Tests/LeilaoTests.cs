7ad2b805959229338505459f78137a8c3fd43e70
Raphael Campos <raphael.campos@streamer.com.br>
2021-10-21 22:51:17 -0300

    02 - Organizando seus testes video 03

--- AluraTestsCourse.LeilaoOnline.Tests/LeilaoTests.cs
+++ AluraTestsCourse.LeilaoOnline.Tests/LeilaoTests.cs
@@ -11,6 +11,55 @@ namespace AluraTestsCourse.LeilaoOnline.Tests
     public class LeilaoTests
     {
         [Fact]
+        public void LeilaoComTresClientes()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var luciano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var joana = new Interessada("Joana", leilao);
+
+            leilao.RecebeLance(maria, 700);
+            leilao.RecebeLance(luciano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(luciano, 1000);
+            leilao.RecebeLance(joana, 1400);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1400;
+            var valorObtido = leilao.Ganhador.Valor;
+            
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(joana, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadorPorValor()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(maria, 700);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);          
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
         public void LeilaoComVariosLances()
         {
             //Arranje - cenário
@@ -27,7 +76,7 @@ namespace AluraTestsCourse.LeilaoOnline.Tests
             leilao.TerminaPregao();
 
             //Assert
-            var valorEsperado = 800;
+            var valorEsperado = 1000;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
